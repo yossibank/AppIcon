6893167f9d7c70ea3f2695ec61174e1600adda34
yossibank <yossibank@yahoo.co.jp>
2025-06-27 22:53:14 +0900

    Initial Commit

---
+++ AppIcon/AppIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconApp.swift
+//  AppIcon
+//
+//  Created by Kamiyama Yoshihito on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct AppIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppIcon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppIcon
+//
+//  Created by Kamiyama Yoshihito on 2025/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
